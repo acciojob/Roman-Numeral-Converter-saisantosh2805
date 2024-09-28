f6bdf3a838b09be85238994d190fb7ecb2b2d3d6
saisantosh2805 <saisantosh2000naviri@gmail.com>
2024-09-28 13:37:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 function convertToRoman(num) {
-    const obj = {
-        0: ['M', 1000],
-        1: ['CM', 900],
-        2: ['D', 500],
-        3: ['CD', 400],
-        4: ['C', 100],
-        5: ['XC', 90],
-        6: ['L', 50],
-        7: ['XL', 40],
-        8: ['X', 10],
-        9: ['IX', 9],
-        10: ['V', 5],
-        11: ['IV', 4],
-        12: ['I', 1]
-    };
+    const romanNumerals = [
+        ['M', 1000],
+        ['CM', 900],
+        ['D', 500],
+        ['CD', 400],
+        ['C', 100],
+        ['XC', 90],
+        ['L', 50],
+        ['XL', 40],
+        ['X', 10],
+        ['IX', 9],
+        ['V', 5],
+        ['IV', 4],
+        ['I', 1]
+    ];
 
     let result = '';
 
-    for (let key of Object.keys(obj).reverse()) {
-        let [symbol, value] = obj[key];
+    for (let i = 0; i < romanNumerals.length; i++) {
+        let [symbol, value] = romanNumerals[i];
         while (num >= value) {
             result += symbol;
             num -= value;
@@ -28,4 +28,4 @@ function convertToRoman(num) {
 }
 
 console.log(convertToRoman(798));  // Outputs: DCCXCVIII
-console.log(convertToRoman(14));   // Outputs: XIV
+console.log(convertToRoman(14)); 
