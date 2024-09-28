9167f7450c6bb5828e280e25aa348550d78051eb
saisantosh2805 <saisantosh2000naviri@gmail.com>
2024-09-28 13:24:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 function convertToRoman(num) {
     const obj = {
         0: ['M', 1000],
-        1: ['D', 500],
-        2: ['C', 100],
-        3: ['L', 50],
-        4: ['X', 10],
-        5: ['V', 5],
-        6: ['I', 1]
+        1: ['CM', 900],
+        2: ['D', 500],
+        3: ['CD', 400],
+        4: ['C', 100],
+        5: ['XC', 90],
+        6: ['L', 50],
+        7: ['XL', 40],
+        8: ['X', 10],
+        9: ['IX', 9],
+        10: ['V', 5],
+        11: ['IV', 4],
+        12: ['I', 1]
     };
 
     let result = '';
 
-    // Iterate over the keys of the object
-    for (let key of Object.keys(obj)) {
-        // Use the key to access the symbol and value
+    for (let key of Object.keys(obj).reverse()) {
         let [symbol, value] = obj[key];
         while (num >= value) {
             result += symbol;
@@ -23,4 +27,5 @@ function convertToRoman(num) {
     return result;
 }
 
-console.log(convertToRoman(36));
+console.log(convertToRoman(798));  // Outputs: DCCXCVIII
+console.log(convertToRoman(14));
