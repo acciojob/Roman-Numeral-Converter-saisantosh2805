0d1813b3ea55925dd0887c78d1239c83a66f6eec
saisantosh2805 <saisantosh2000naviri@gmail.com>
2024-09-28 13:22:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
+    const obj = {
+        0: ['M', 1000],
+        1: ['D', 500],
+        2: ['C', 100],
+        3: ['L', 50],
+        4: ['X', 10],
+        5: ['V', 5],
+        6: ['I', 1]
     };
 
-    let result = '';  // Initialize the result variable
+    let result = '';
 
-    // Convert obj into an array of entries and iterate over it
-    for(let [symbol, value] of Object.values(obj)){
+    // Iterate over the keys of the object
+    for (let key of Object.keys(obj)) {
+        // Use the key to access the symbol and value
+        let [symbol, value] = obj[key];
         while (num >= value) {
             result += symbol;
             num -= value;
